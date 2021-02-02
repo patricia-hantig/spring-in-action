ee4a2471e12e74f6a1ab35335f16d0928da61fc2
Patri <patricia.laura.hantig@gmail.com>
2021-02-02 14:59:34 -0800

    chapter 3 part 2

--- taco-cloud/src/main/java/com/patricia/tacocloud/Order.java
+++ taco-cloud/src/main/java/com/patricia/tacocloud/Order.java
@@ -3,15 +3,27 @@ package com.patricia.tacocloud;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;            // field that uniquely identifies the object
+
+    private Date placedAt;     // filed when an Order is placed
 
     @NotBlank(message = "Delivery Name is required")
     private String deliveryName;
@@ -38,14 +50,16 @@ public class Order {
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
-    private Long id;            // field that uniquely identifies the object
-
-    private Date placedAt;     // filed when an Order is placed
-
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco design) {
         this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
     }
 }
 
@@ -56,3 +70,16 @@ public class Order {
 // @Pattern(regex="...') - is used to create pattern "MM/YY" for expiration date of the credit card
 // @Digits(integer=3, fraction=0, message="...") - property's value is exact 3 digits otherwise message is displayed
 // - except @CreditCardNumber all the other annotations are located within javax.validation.constants package
+
+// ■■ Annotations chapter3_part_2
+// @Entity  = marks the class as a JPA entity
+//          - an entity represents a table in a relational database
+//          - by have the class as Entity - we don't need to manually create the table using schema.sql - it creates the table automatically & also the needed tables for solving a manyToMany relationship
+// @Table   = specifies that Order entities should be persisted to Taco_Order table in database
+//          - it's necessary with Order because without it, JPA would default to persisting the entities to a table named Order
+//          (order is a reserved word in SQL)
+// @Id = the property will uniquely identify the entity in the database
+// @GeneratedValue(strategy = GenerationType.AUTO) = because we use a database which automatically generates the id value
+// @ManyToMany(targetEntity = Ingredient.class) - declare the relationship between an Order & its associated Taco list
+//                                              = An Order can have many Taco objects & an Taco can be part of many Orders
+// @PrePersist - we use it to set the property placedAt to the current date and time before Order is persisted
--- taco-cloud/src/main/java/com/patricia/tacocloud/data/OrderRepository.java
+++ taco-cloud/src/main/java/com/patricia/tacocloud/data/OrderRepository.java
@@ -1,8 +1,13 @@
 package com.patricia.tacocloud.data;
 
 import com.patricia.tacocloud.Order;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 
-    Order save(Order order);
 }
+
+// CrudRepository declares about a dozen methods for CRUD operations
+// CrudRepository it's parameterized - the first param is the entity type the repository will persist & second param is the type of the entity ID property
+// with Spring Data JPA - there is no need to write an implementation - when the application starts, Spring Data JPA automatically generates an implementation
+// we just need to inject them into the controllers like we did for JDBC-based implementation
